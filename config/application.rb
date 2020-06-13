62393965aef979c0b622217ecb15c01bb7508d57
Frederic Crozatier <frederic.crozatier@protonmail.com>
2020-06-13 11:16:54 +0200

    Commented 'action dispatch x sendfile header'

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,5 @@ module Sfantasy
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # config.assets.initialize_on_precompile = false                               # added
-
   end
 end
